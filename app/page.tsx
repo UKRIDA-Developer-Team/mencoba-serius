425652116cd676f863505a9c5726626e32cd23b9
DavidsonRafaelK <davidson.rafael120@gmail.com>
2026-04-08 20:32:11 +0700

    feat(order layout): adding card, button changed next.config.ts to allow external image remote protocol from unsplash.com

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,110 @@
+"use client";
+
+import { CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+const Greetings = [
+  "Good Morning",
+  "Good Afternoon",
+  "Good Evening",
+]
+
+const getGreeting = (): string => {
+  const currentHour = new Date().getHours();
+  
+  if (currentHour < 12) {
+    return Greetings[0];
+  } else if (currentHour < 18) {
+    return Greetings[1];
+  } else {
+    return Greetings[2];
+  }
+}
+
+const menuItems = [
+  {
+    name: "Bika Ambon",
+    description: "Delicacy premium dengan resep autentik & bahan pilihan berkualitas tinggi",
+    price: "Rp 50.000",
+    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kb25lc2lhbiUyMGNhbmtlfGVufDB8fDB8fHww&w=1000&q=80"
+  },
+  {
+    name: "Kue Cubit",
+    description: "Produk artisanal istimewa dengan presentasi modern & cita rasa superior",
+    price: "Rp 30.000",
+    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kb25lc2lhbiUyMGNhbmtlfGVufDB8fDB8fHww&w=1000&q=80"
+  },
+  {
+    name: "Kue Cubit",
+    description: "Produk artisanal istimewa dengan presentasi modern & cita rasa superior",
+    price: "Rp 30.000",
+    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kb25lc2lhbiUyMGNhbmtlfGVufDB8fDB8fHww&w=1000&q=80"
+  },
+  {
+    name: "Kue Cubit",
+    description: "Produk artisanal istimewa dengan presentasi modern & cita rasa superior",
+    price: "Rp 30.000",
+    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kb25lc2lhbiUyMGNhbmtlfGVufDB8fDB8fHww&w=1000&q=80"
+  },
+  {
+    name: "Kue Cubit",
+    description: "Produk artisanal istimewa dengan presentasi modern & cita rasa superior",
+    price: "Rp 30.000",
+    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kb25lc2lhbiUyMGNhbmtlfGVufDB8fDB8fHww&w=1000&q=80"
+  },
+  {
+    name: "Kue Cubit",
+    description: "Produk artisanal istimewa dengan presentasi modern & cita rasa superior",
+    price: "Rp 30.000",
+    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kb25lc2lhbiUyMGNhbmtlfGVufDB8fDB8fHww&w=1000&q=80"
+  },
+  {
+    name: "Kue Cubit",
+    description: "Produk artisanal istimewa dengan presentasi modern & cita rasa superior",
+    price: "Rp 30.000",
+    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kb25lc2lhbiUyMGNhbmtlfGVufDB8fDB8fHww&w=1000&q=80"
+  }
+]
+
+
 export default function Home() {
+  const greeting = getGreeting();
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+        <div className="w-full max-w-6xl">
+          <h1 className="text-4xl font-bold">What We Serve</h1>
+          <span className="text-sm text-slate-600">{greeting}</span>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="w-full max-w-6xl mt-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+            {menuItems.map((item, index) => (
+              <div key={index} className="w-full max-w-xs">
+                <div className="overflow-hidden bg-white border flex flex-col h-full">
+                  <div className="relative h-48 w-full bg-slate-100">
+                    <Image
+                      src={item.imageUrl}
+                      alt={item.name}
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
+                  <CardHeader className="pb-0 pt-3 px-4 flex-1">
+                    <CardTitle className="text-base">{item.name}</CardTitle>
+                    <CardDescription className="line-clamp-2 text-xs text-slate-600 my-3">
+                      {item.description}
+                    </CardDescription>
+                  </CardHeader>
+                  <CardFooter className="flex items-center justify-between pt-4 px-4 pb-3">
+                    <span className="font-semibold text-slate-900">{item.price}</span>
+                    <Button size="sm">Add to Cart</Button>
+                  </CardFooter>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </main>
-    </div>
+    </main>
   );
 }
